9208952fbc6c2772ad5248cecc27a39bc0b2aff7
Akash <krishnanag1996@gmail.com>
2021-01-13 22:33:38 +0530

    grid

--- fib_memo.js
+++ fib_memo.js
@@ -1,10 +1,10 @@
-const fib = (n, memo = {}) => {
+const fib = (n, memo = {1: 1, 2: 1}) => {
   if (n in memo) return memo[n];
-  if (n <= 2) return 1;
+  
   memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
   return memo[n];
 };
 console.log(fib(6));
 console.log(fib(7));
 console.log(fib(8));
-console.log(fib(12));
+console.log(fib(10));
